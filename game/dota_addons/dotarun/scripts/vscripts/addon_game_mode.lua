73033d11a4fb70f7f641baaf59df7ebd4a872c1c
Okokookokok <mathiastj0@gmail.com>
2014-08-31 13:28:33 +0200

    t stuff

--- game/dota_addons/dotarun/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/dotarun/scripts/vscripts/addon_game_mode.lua
@@ -1,13 +1,12 @@
 -- Generated from template
 
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+
+if CDotaRun == nil then
+	CDotaRun = class({})
 end
 
 function Precache( context )
-	PrecacheUnitByNameSync("npc_dota_hero_venomancer", context) --[[Returns:void
-	Precaches a DOTA unit by its dota_npc_units.txt name
-	]]
+	PrecacheUnitByNameSync("npc_dota_hero_venomancer", context)
 	--[[
 		Precache things we know we'll use.  Possible file types include (but not limited to):
 			PrecacheResource( "model", "*.vmdl", context )
@@ -19,59 +18,30 @@ end
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
-	GameRules.AddonTemplate:InitGameMode()
+	print("init")
+	GameRules.dotaRun = CDotaRun()
+	GameRules.dotaRun:InitGameMode()
 end
 
-function CAddonTemplateGameMode:InitGameMode()
+function CDotaRun:InitGameMode()
+	self.waypoints = {}
+	for i = 0, 9 do
+    self.waypoints[i] = {}
+
+    	for j = 1, 3 do
+        	self.waypoints[i][j] = false -- Fill the values here
+    	end
+	end
 	print( "Template addon is loaded." )
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
-	--ListenToGameEvent("dota_player_used_ability", Dynamic_Wrap(CAddonTemplateGameMode, 'On_dota_player_used_ability'), self)
-	ListenToGameEvent("dota_action_item", Dynamic_Wrap(CAddonTemplateGameMode, 'On_dota_action_item'), self)
-	ListenToGameEvent("dota_item_used", Dynamic_Wrap(CAddonTemplateGameMode, 'On_dota_item_used'), self)
-	ListenToGameEvent("dota_inventory_item_changed", Dynamic_Wrap(CAddonTemplateGameMode, 'On_dota_inventory_item_changed'), self)
-		ListenToGameEvent("dota_inventory_item_added", Dynamic_Wrap(CAddonTemplateGameMode, 'On_dota_inventory_item_added'), self)
-
-
-
-
 end
 
 -- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
+function CDotaRun:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		--print( "Template addon script is running." )
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
 	return 1
-end
-
---[[function CAddonTemplateGameMode:On_dota_player_used_ability(data)
-	table.foreach(data, print) 
-	print(data["PlayerID"])
-	player = PlayerResource:GetPlayer(data["PlayerID"]-1)
-	print(player)
-	print(data["abilityname"])
-	player:GetAssignedHero():RemoveAbility(data["abilityname"])
-end
-]]
-function CAddonTemplateGameMode:On_dota_action_item(data)
-	print("Item action")
-	table.foreach(data, print) 
- end
-
- function CAddonTemplateGameMode:On_dota_item_used(data)
-  print("[BAREBONES] dota_item_used")
-  table.foreach(data, print) 
-end
-
- function CAddonTemplateGameMode:On_dota_inventory_item_changed(data)
-  print("[BAREBONES] item changed")
-  table.foreach(data, print) 
-end
-
- function CAddonTemplateGameMode:On_dota_inventory_item_added(data)
-  print("[BAREBONES] item added")
-  table.foreach(data, print) 
 end
---
+++ game/dota_addons/dotarun/scripts/vscripts/waypoint.lua
@@ -0,0 +1,213 @@
+
+
+function WaypointOneTouch(trigger)
+     
+    playerID = trigger.activator:GetPlayerID() 
+    print(playerID)
+    GameRules.dotaRun.waypoints[playerID][1] = true
+    print(GameRules.dotaRun.waypoints[playerID][1])
+    print(GameRules.dotaRun.waypoints[playerID][2])
+    print(GameRules.dotaRun.waypoints[playerID][3])
+
+            
+            -- if (trigger.activator:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+            --     playerID = trigger.activator:GetPlayerID() 
+            --     player = PlayerResource:GetPlayer(playerID)
+            --     hero = player:GetAssignedHero() 
+            --     GameRules:MakeTeamLose(3)
+
+            --     --hero:AddExperience(5000, false)
+
+            --     hero:AddItem(CreateItem("item_ancient_janggo" ,hero ,hero)) 
+            --     print("Trying to create item")
+                
+            --     --hero:AddItem(item_blink) 
+            --     --trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+
+            -- else
+            --     trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+            --     print("Is not owned by player - Terminate")
+                
+            -- end  
+
+           
+end	
+
+function WaypointTwoTouch(trigger)
+
+
+    playerID = trigger.activator:GetPlayerID() 
+    print(playerID)      
+    GameRules.dotaRun.waypoints[playerID][2] = true
+    print(GameRules.dotaRun.waypoints[playerID][1])
+    print(GameRules.dotaRun.waypoints[playerID][2])
+    print(GameRules.dotaRun.waypoints[playerID][3])
+     
+          
+            
+    --         if (trigger.activator:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+    --             playerID = trigger.activator:GetPlayerID() 
+    --             player = PlayerResource:GetPlayer(playerID)
+    --             hero = player:GetAssignedHero() 
+    --             GameRules:MakeTeamLose(3)
+
+    --             --hero:AddExperience(5000, false)
+
+    --             hero:AddItem(CreateItem("item_ancient_janggo" ,hero ,hero)) 
+    --             print("Trying to create item")
+                
+    --             --hero:AddItem(item_blink) 
+    --             --trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+
+    --         else
+    --             trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+    --             print("Is not owned by player - Terminate")
+                
+    --         end  
+
+           
+end 
+
+function WaypointThreeTouch(trigger)
+
+    playerID = trigger.activator:GetPlayerID() 
+    print(playerID)      
+    GameRules.dotaRun.waypoints[playerID][3] = true
+    print(GameRules.dotaRun.waypoints[playerID][1])
+    print(GameRules.dotaRun.waypoints[playerID][2])
+    print(GameRules.dotaRun.waypoints[playerID][3])
+     
+end           
+            
+            -- if (trigger.activator:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+            --     playerID = trigger.activator:GetPlayerID() 
+            --     player = PlayerResource:GetPlayer(playerID)
+            --     hero = player:GetAssignedHero() 
+            --     GameRules:MakeTeamLose(3)
+
+            --     --hero:AddExperience(5000, false)
+
+            --     hero:AddItem(CreateItem("item_ancient_janggo" ,hero ,hero)) 
+            --     print("Trying to create item")
+                
+            --     --hero:AddItem(item_blink) 
+            --     --trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+
+            -- else
+            --     trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+            --     print("Is not owned by player - Terminate")
+                
+            -- end  
+
+           
+
+
+function WinHere(trigger)
+
+
+    playerID = trigger.activator:GetPlayerID()
+    player = PlayerResource:GetPlayer(playerID)
+    hero = player:GetAssignedHero()  
+
+    print("WinHere")
+    print(playerID)
+    print(GameRules.dotaRun.waypoints[playerID][1])
+    print(GameRules.dotaRun.waypoints[playerID][2])
+    print(GameRules.dotaRun.waypoints[playerID][3])
+    print(GameRules.dotaRun.waypoints[playerID][1] and GameRules.dotaRun.waypoints[playerID][2] and GameRules.dotaRun.waypoints[playerID][3])
+    
+
+    if (GameRules.dotaRun.waypoints[playerID][1] and GameRules.dotaRun.waypoints[playerID][2] and GameRules.dotaRun.waypoints[playerID][3]) then
+        if (hero:GetTeamNumber() == 3) then
+            GameRules:MakeTeamLose(2)
+        elseif (hero:GetTeamNumber() == 2) then
+            GameRules:MakeTeamLose(3)
+        end
+    end
+end 
+           
+        -- if (trigger.activator:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+        --        playerID = trigger.activator:GetPlayerID() 
+        --        player = PlayerResource:GetPlayer(playerID)
+        --        hero = player:GetAssignedHero() 
+        --        GameRules:MakeTeamLose(3)
+
+        --         --hero:AddExperience(5000, false)
+
+        --        hero:AddItem(CreateItem("item_ancient_janggo" ,hero ,hero)) 
+        --        print("Trying to create item")
+                
+        --         --hero:AddItem(item_blink) 
+        --         --trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+
+        -- else
+        --     trigger.activator:SetGold(trigger.activator:GetGold() + 5000, true) 
+        --     print("Is not owned by player - Terminate")
+                
+        -- end  
+
+           
+
+
+
+
+
+
+
+
+    -- function OnStartTouch(trigger)
+ 
+ --        print(trigger.activator)
+ --        print(trigger.caller)
+
+ --        --unitName = trigger:GetUnitName() -- Retrieves the name that the unit has, such as listed in "npc_units_custom.txt"
+
+
+ --        --print("Unit '" .. unitName .. "' has entered the killbox")
+
+ --        hero = trigger.activator:GetAssignedHero()
+ --        hero:AddItem(item_blink) --[[Returns:void
+ --        Add an item to this unit's inventory.
+ --        ]]
+
+ --        if (trigger.activator:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+ --            trigger.activator:ForceKill(true) -- Kills the unit
+ --            print("Is player owned - Ignore")
+
+ --        else
+ --            print("Is not owned by player - Terminate")
+ --            trigger.activator:ForceKill(true) -- Kills the unit
+ --        end   
+       
+ --    end
+
+
+
+    -- function Waypoint1(trigger)
+     
+    --         print(trigger.activator)
+    --         print(trigger.caller)
+
+           
+    -- end
+     
+    -- function Waypoint2(trigger)
+     
+    -- --     print(trigger.activator)
+    -- --     print(trigger.caller)
+
+   	-- 	unitName = trigger:GetUnitName() -- Retrieves the name that the unit has, such as listed in "npc_units_custom.txt"
+
+    -- 	print("Unit '" .. unitName .. "' has entered the killbox")
+
+    -- 	if (key:IsOwnedByAnyPlayer() ) then -- Checks to see if the entity is a player controlled unit
+    -- 		key:ForceKill(true) -- Kills the unit
+    --    		print("Is player owned - Ignore")
+
+    -- 	else
+    --     	print("Is not owned by player - Terminate")
+    --     	key:ForceKill(true) -- Kills the unit
+    -- 	end        
+           
+    -- end
+
