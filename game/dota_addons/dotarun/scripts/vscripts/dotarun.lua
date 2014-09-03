3e9ba9b60c0fdff414717609e2fadc111206f26e
mathiastj <2278040+mathiastj@users.noreply.github.com>
2014-09-03 16:23:34 +0200

    havde ret travlt, men som du kan se virker astral imprisonment nu, de skal altså ligge i den anden fil, kig i houldout_example hvis du er i tvivl

--- game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
+++ game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
@@ -1,8 +1,9 @@
 local zoneCount = {}
 
 itemList = { "item_blink", "item_force_staff", "item_cyclone", "item_shivas_guard", "item_sheepstick"}
-spellList = {"mirana_arrow", "mirana_leap", "venomancer_venomous_gale", "dark_seer_surge", "clinkz_wind_walk", "jakiro_ice_path", 
-"batrider_flamebreak", "ancient_apparition_ice_vortex", "gyrocopter_homing_missile", "obsidian_destroyer_astral_imprisonment"}
+-- spellList = {"mirana_arrow", "mirana_leap", "venomancer_venomous_gale", "dark_seer_surge", "clinkz_wind_walk", "jakiro_ice_path", 
+-- "batrider_flamebreak", "ancient_apparition_ice_vortex", "gyrocopter_homing_missile", "obsidian_destroyer_astral_imprisonment"}
+spellList = {"obsidian_destroyer_astral_imprisonment_custom"}
 
 --problems:
 --alchemist_unstable_concoction
@@ -17,7 +18,8 @@ function GiveRandomItem(hero)
 end
 
 function GiveRandomAbility(hero)
-	abilityName = spellList[math.random(#spellList)]
+	-- abilityName = spellList[math.random(#spellList)]
+	abilityName = "obsidian_destroyer_astral_imprisonment_custom"
 	print("Rolled ability "..abilityName)
 	if(hero:FindAbilityByName(abilityName) == nil) then
         hero:RemoveAbility("mirana_fart") 
