5525d83030647d1ab26e896baf1e768ce1f5310a
T <mathiastj0@gmail.com>
2014-09-04 09:01:59 +0200

    all spells are in, but no particles :( maybe precache by hero

--- game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
+++ game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
@@ -1,9 +1,8 @@
 local zoneCount = {}
 
 itemList = { "item_blink", "item_force_staff", "item_cyclone", "item_shivas_guard", "item_sheepstick"}
--- spellList = {"mirana_arrow", "mirana_leap", "venomancer_venomous_gale", "dark_seer_surge", "clinkz_wind_walk", "jakiro_ice_path", 
--- "batrider_flamebreak", "ancient_apparition_ice_vortex", "gyrocopter_homing_missile", "obsidian_destroyer_astral_imprisonment"}
-spellList = {"obsidian_destroyer_astral_imprisonment_custom"}
+spellList = {"mirana_arrow_custom", "mirana_leap_custom", "venomancer_venomous_gale_custom", "dark_seer_surge_custom", "jakiro_ice_path_custom", 
+"batrider_flamebreak_custom", "ancient_apparition_ice_vortex_custom", "gyrocopter_homing_missile_custom", "obsidian_destroyer_astral_imprisonment_custom"}
 
 --problems:
 --alchemist_unstable_concoction
@@ -18,8 +17,8 @@ function GiveRandomItem(hero)
 end
 
 function GiveRandomAbility(hero)
-	-- abilityName = spellList[math.random(#spellList)]
-	abilityName = "obsidian_destroyer_astral_imprisonment_custom"
+	abilityName = spellList[math.random(#spellList)]
+	-- abilityName = "obsidian_destroyer_astral_imprisonment_custom"
 	print("Rolled ability "..abilityName)
 	if(hero:FindAbilityByName(abilityName) == nil) then
         hero:RemoveAbility("mirana_fart") 
