56ae2a16dfa7af961c31b7ca78fa32de658a1d9a
Okokookokok <mathiastj0@gmail.com>
2014-09-07 14:36:55 +0200

    Fixed abilities and more, look in comment

--- game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
+++ game/dota_addons/dotarun/scripts/vscripts/dotarun.lua
@@ -1,7 +1,11 @@
 
 local zoneCount = {}
+for i = 0,9 do
+	zoneCount[i] = true
+end
 
-itemList = { "item_blink", "item_force_staff", "item_cyclone", "item_shivas_guard", "item_sheepstick", "item_ancient_janggo", "item_smoke_of_deceit", "item_rod_of_atos"}
+
+itemList = { "item_blink", "item_cyclone", "item_shivas_guard", "item_sheepstick", "item_ancient_janggo", "item_smoke_of_deceit", "item_rod_of_atos"}
 spellList = {"mirana_arrow_custom", "mirana_leap_custom", "venomancer_venomous_gale_custom", "dark_seer_surge_custom", "jakiro_ice_path_custom", 
 "batrider_flamebreak_custom", "ancient_apparition_ice_vortex_custom", "gyrocopter_homing_missile_custom", "obsidian_destroyer_astral_imprisonment_custom"}
 
@@ -12,23 +16,65 @@ spellList = {"mirana_arrow_custom", "mirana_leap_custom", "venomancer_venomous_g
 
 function GiveRandomItem(hero)
  	
-	item = CreateItem(itemList[math.random(#itemList)], hero, hero)
-	hero:AddItem(item)
+	itemNew = CreateItem(itemList[math.random(#itemList)], hero, hero)
+	alreadyHas = false
+	for i=0,5 do 
+	   	itemOld = hero:GetItemInSlot(i)
+		if(itemOld ~= nil and itemOld:GetClassname() == itemNew:GetClassname()) then
+			print("Hero already has: " .. itemNew:GetClassname())
+			alreadyHas = true
+			break
+		elseif (itemNew:GetClassname() == "item_smoke_of_deceit" and hero:GetPlayerID() == GameRules.dotaRun.lead) then
+			print("Cannot get smoke if you're in the lead")
+			alreadyHas = true
+	    end
+	end
+
+	if (alreadyHas) then 
+		GiveRandomItem(hero)
+	else
+		print("Adding item: " .. itemNew:GetClassname())
+	    hero:AddItem(itemNew)
+	end
 
 end
 
 function GiveRandomAbility(hero)
 
-	abilityName = spellList[math.random(#spellList)]
-	-- abilityName = "obsidian_destroyer_astral_imprisonment_custom"
-	print("Rolled ability "..abilityName)
-	if(hero:FindAbilityByName(abilityName) == nil) then
-        hero:RemoveAbility("mirana_fart") 
-		hero:AddAbility(abilityName)
-		ability = hero:FindAbilityByName(abilityName)
-		ability:SetLevel(1)
-		ability:SetAbilityIndex(1)
-    end
+	ability1 = hero:GetAbilityByIndex(1)
+	ability2 = hero:GetAbilityByIndex(2)
+	ability3 = hero:GetAbilityByIndex(3)
+	ability4 = hero:GetAbilityByIndex(4)
+	ability5 = hero:GetAbilityByIndex(5)
+	ability6 = hero:GetAbilityByIndex(6)
+
+	print(ability1:GetAbilityName() .. " " .. ability2:GetAbilityName() .. " " .. ability3:GetAbilityName() .. 
+		" " .. ability4:GetAbilityName() .. " " .. ability5:GetAbilityName() .. " " .. ability6:GetAbilityName())
+	
+	hasMaxAbilities = false
+	if(ability1:GetAbilityName() ~= "empty_ability1" and ability2:GetAbilityName() ~= "empty_ability1" and ability3:GetAbilityName() ~= "empty_ability1" and
+		ability4:GetAbilityName() ~= "empty_ability1" and ability5:GetAbilityName() ~= "empty_ability1" and ability6:GetAbilityName() ~= "empty_ability1") then
+		print("Hero already has six abilities")
+		hasMaxAbilities = true
+	else 
+		hasMaxAbilities = false
+	end
+
+	if (not hasMaxAbilities) then
+		abilityName = spellList[math.random(#spellList)]
+		if(hero:FindAbilityByName(abilityName) == nil) then
+			print("Adding ability: "..abilityName)
+    	    hero:RemoveAbility("empty_ability1") 
+			hero:AddAbility(abilityName)
+			ability = hero:FindAbilityByName(abilityName)
+			ability:SetLevel(1)
+			-- ability:SetAbilityIndex(1)
+		else 
+			print("Hero already had ability: "..abilityName)
+			GiveRandomAbility(hero)
+   		end
+	end
+
 end
 
 function ItemZoneOne(trigger)
@@ -39,25 +85,36 @@ function ItemZoneOne(trigger)
 	math.random()
 	math.random()
 
-	print("Entered Item Zone")
-
-
 	hero = trigger.activator
+
+	print("Entered Item Zone")
+	print(zoneCount[hero:GetPlayerID()])
 
 
 	
-	 GiveRandomAbility(hero)
 
 
-	if(zoneCount[hero:GetPlayerID()] == nil) then
-		zoneCount[hero:GetPlayerID()] = 0
-	end
-
-	if(zoneCount[hero:GetPlayerID()] < 5) then
-		zoneCount[hero:GetPlayerID()] = zoneCount[hero:GetPlayerID()] + 1
-		print("items picked up by player " .. hero:GetPlayerID()..": "..zoneCount[hero:GetPlayerID()])
+	if (zoneCount[hero:GetPlayerID()] == true) then
+		GiveRandomAbility(hero)
 		GiveRandomItem(hero)
-	end
+		zoneCount[hero:GetPlayerID()] = false
+		Timers:CreateTimer(5, function()
+			zoneCount[hero:GetPlayerID()] = true
+            return
+        end
+        )
+    end
+
+
+	-- if(zoneCount[hero:GetPlayerID()] == nil) then
+	-- 	zoneCount[hero:GetPlayerID()] = 0
+	-- end
+
+	-- if(zoneCount[hero:GetPlayerID()] < 5) then
+	-- 	zoneCount[hero:GetPlayerID()] = zoneCount[hero:GetPlayerID()] + 1
+	-- 	print("items picked up by player " .. hero:GetPlayerID()..": "..zoneCount[hero:GetPlayerID()])
+	-- 	GiveRandomItem(hero)
+	-- end
 end
 
 function WaterSlow(trigger)
