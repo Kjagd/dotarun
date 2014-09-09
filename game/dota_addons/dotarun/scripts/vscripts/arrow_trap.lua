1c68b375314c3023c10595ba90f27ec6c3cbc8fb
mathiastj <2278040+mathiastj@users.noreply.github.com>
2014-09-09 20:00:01 +0200

    More arrows, more water

--- game/dota_addons/dotarun/scripts/vscripts/arrow_trap.lua
+++ game/dota_addons/dotarun/scripts/vscripts/arrow_trap.lua
@@ -6,3 +6,35 @@ function Fire(data)
     	thisEntity:CastAbilityOnPosition(target:GetOrigin(), arrow, -1 )
   	end
 end
+
+
+local Points = {Vector(4058,6076,20),Vector(3286,6964,20),Vector(2286,6072,20),Vector(1470,6940,20), Vector(-1120,6724,20), Vector(-1097,6528,20), Vector(-1114,6361,20)}
+function FireAtPoint(data)
+	print("Arrow firing")
+	-- DeepPrintTable(data)
+	local target = data.caller
+	if target ~= nil then
+		if(string.find(thisEntity:GetName(), "two")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[1], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "three")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[2], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "four")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[3], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "five")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[4], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "six")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[5], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "seven")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[6], arrow, -1 )
+    	elseif (string.find(thisEntity:GetName(), "eight")) then
+    		local arrow = thisEntity:FindAbilityByName("mirana_arrow_custom")
+    		thisEntity:CastAbilityOnPosition(Points[7], arrow, -1 )
+    	end
+  	end
+end
