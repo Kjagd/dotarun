9c2ec6cf8162f9d0a900036711bd1311e0f40947
mathiastj <2278040+mathiastj@users.noreply.github.com>
2014-09-10 21:33:55 +0200

    better restart, fix trap

--- game/dota_addons/dotarun/scripts/vscripts/waypoint.lua
+++ game/dota_addons/dotarun/scripts/vscripts/waypoint.lua
@@ -11,7 +11,7 @@ function WaypointOneTouch(trigger)
     -- print(playerID)
     GameRules.dotaRun.waypoints[playerID][1] = true
     
-    lastMan(1)
+    lastMan(1, trigger.activator)
 
     -- print(GameRules.dotaRun.waypoints[playerID][1])
     -- print(GameRules.dotaRun.waypoints[playerID][2])
@@ -55,7 +55,7 @@ function WaypointTwoTouch(trigger)
     	GameRules.dotaRun.waypoints[playerID][2] = true
     end
 
-    lastMan(2)
+    lastMan(2, trigger.activator)
 
     
     -- print(GameRules.dotaRun.waypoints[playerID][1])
@@ -100,7 +100,7 @@ function WaypointThreeTouch(trigger)
     	GameRules.dotaRun.waypoints[playerID][3] = true
     end
 
-    lastMan(3)
+    lastMan(3, trigger.activator)
 
 
     -- print(GameRules.dotaRun.waypoints[playerID][1])
@@ -129,19 +129,17 @@ end
                 
             -- end  
 
-function lastMan(waypointID)
-    troughCount = 0
+function lastMan(waypointID, hero)
+    throughCount = 0
     for i = 0, 9 do
-       if (GameRules.dotaRun.waypoints[i][waypointID]) then
-         troughCount = troughCount + 1
+        if (GameRules.dotaRun.waypoints[i][waypointID]) then
+            throughCount = throughCount + 1
         end
     end
 
-    if (troughCount == GameRules.dotaRun.playerCount) then
+    if (throughCount == GameRules.dotaRun.playerCount) then
         print("last man!")
         abilityName = "gyrocopter_homing_missile_custom"
-        player = PlayerResource:GetPlayer(playerID)
-        hero = player:GetAssignedHero() 
         if(hero:FindAbilityByName(abilityName) == nil) then
             print("Adding ability: "..abilityName)
             hero:RemoveAbility("empty_ability1") 
@@ -169,20 +167,20 @@ function WinHere(trigger)
 
     if (GameRules.dotaRun.waypoints[playerID][1] and GameRules.dotaRun.waypoints[playerID][2] and GameRules.dotaRun.waypoints[playerID][3]) then
         if (hero:GetTeamNumber() == DOTA_TEAM_BADGUYS) then
-            if (GameRules.dotaRun.laps[DOTA_TEAM_BADGUYS] == 1) then
+            if (GameRules.dotaRun.badLaps == 1) then
                 GameRules:SetSafeToLeave( true )
                 GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
             else 
-                GameRules.dotaRun.laps[DOTA_TEAM_BADGUYS] = GameRules.dotaRun.laps[DOTA_TEAM_BADGUYS] + 1
+                GameRules.dotaRun.badLaps = GameRules.dotaRun.badLaps + 1
                 NewLap(DOTA_TEAM_BADGUYS)
             end
 
         elseif (hero:GetTeamNumber() == DOTA_TEAM_GOODGUYS) then
-            if (GameRules.dotaRun.laps[DOTA_TEAM_GOODGUYS] == 1) then
+            if (GameRules.dotaRun.goodLaps == 1) then
                 GameRules:SetSafeToLeave( true )
                 GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
             else
-                GameRules.dotaRun.laps[DOTA_TEAM_GOODGUYS] = GameRules.dotaRun.laps[DOTA_TEAM_GOODGUYS] + 1
+                GameRules.dotaRun.goodLaps = GameRules.dotaRun.goodLaps + 1
                 NewLap(DOTA_TEAM_GOODGUYS)
             end
         end
@@ -198,8 +196,8 @@ end
 
 function KillEntity(trigger)
 
-    unitName = trigger.activator:GetUnitName() -- Retrieves the name that the unit has, such as listed in "npc_units_custom.txt"
-    playerID = trigger.activator:GetPlayerID()
+    local unitName = trigger.activator:GetUnitName() -- Retrieves the name that the unit has, such as listed in "npc_units_custom.txt"
+    local playerID = trigger.activator:GetPlayerID()
 
     print("Unit '" .. unitName .. "' has entered the killbox")
 
@@ -227,7 +225,7 @@ function KillEntity(trigger)
             -- Stop the hero, so he doesn't move
             trigger.activator:Stop()
             -- Refocus the camera of said player to the position of the teleported hero.
-            SendToConsole("dota_camera_center")
+            PlayerResource:SetCameraTarget(playerID, trigger.activator)
         else 
             trigger.activator:ForceKill(true) -- Kills the unit
         end
@@ -248,6 +246,7 @@ end
 
 function NewLap(winner)
     --Still needs to kill a hero of opposing team to change scoreboard
+    GameRules.dotaRun:ResetRound()
 
     for i = 0, 9 do
     GameRules.dotaRun.waypoints[i] = {}
@@ -258,40 +257,50 @@ function NewLap(winner)
     end
 
     for i = 0,9 do
+        GameRules.dotaRun.spawned[i] = false
+    end
+
+    for i = 0,9 do
         local player = PlayerResource:GetPlayer(i)
         if (player ~=nil) then
-            local hero = player:GetAssignedHero()
-            if (hero ~=nil) then
-                for j = 1,9 do
-                    abilityName = GameRules.dotaRun.spellList[j]
-                    local ability = hero:FindAbilityByName(abilityName)
-                    if(ability ~= nil) then
-                        ability:SetLevel(0)
-                        print("Level of ability: " .. ability:GetLevel())
-                        hero:RemoveAbility(abilityName)
-                        -- if(hero:FindAbilityByName(data.abilityname) ~= nil) then
-                        --  hero:RemoveAbility("mirana_fart")
-                        -- end
-                        hero:AddAbility("empty_ability1") 
-                    end
-                end
+            PlayerResource:ReplaceHeroWith(i, "npc_dota_hero_mirana", 0, 0)
+            local hero = player:GetAssignedHero() 
+            local point = Entities:FindByName( nil, "waypointHomeTeleport"):GetAbsOrigin()
+            FindClearSpaceForUnit(hero, point, false)
+            hero:AddNewModifier(caster, ability, "modifier_stunned", modifier_table) 
 
-                for j=0,5 do 
-                    local item = hero:GetItemInSlot(j)
-                    if  (item ~= nil and item:GetClassname()  ~= "item_force_staff") then
-                        print("Deleting item: " .. item:GetClassname())
-                        hero:RemoveItem(item)
-                    end
-                end
-                -- if (winner == DOTA_TEAM_BADGUYS) then
-                point = Entities:FindByName( nil, "waypointHomeTeleport" ):GetAbsOrigin()
-                FindClearSpaceForUnit(hero, point, false)
-                -- Stop the hero, so he doesn't move
-                hero:Stop()
-                -- Refocus the camera of said player to the position of the teleported hero.
-                SendToConsole("dota_camera_center")
-                hero:AddNewModifier(caster, ability, "modifier_stunned", modifier_table) 
-            end
+            -- local hero = player:GetAssignedHero()
+            -- if (hero ~=nil) then
+            --     for j = 1,9 do
+            --         abilityName = GameRules.dotaRun.spellList[j]
+            --         local ability = hero:FindAbilityByName(abilityName)
+            --         if(ability ~= nil) then
+            --             ability:SetLevel(0)
+            --             print("Level of ability: " .. ability:GetLevel())
+            --             hero:RemoveAbility(abilityName)
+            --             -- if(hero:FindAbilityByName(data.abilityname) ~= nil) then
+            --             --  hero:RemoveAbility("mirana_fart")
+            --             -- end
+            --             hero:AddAbility("empty_ability1") 
+            --         end
+            --     end
+
+            --     for j=0,5 do 
+            --         local item = hero:GetItemInSlot(j)
+            --         if  (item ~= nil and item:GetClassname()  ~= "item_force_staff") then
+            --             print("Deleting item: " .. item:GetClassname())
+            --             hero:RemoveItem(item)
+            --         end
+            --     end
+            --     -- if (winner == DOTA_TEAM_BADGUYS) then
+            --     point = Entities:FindByName( nil, "waypointHomeTeleport" ):GetAbsOrigin()
+            --     FindClearSpaceForUnit(hero, point, false)
+            --     -- Stop the hero, so he doesn't move
+            --     hero:Stop()
+            --     -- Refocus the camera of said player to the position of the teleported hero.
+            --     SendToConsole("dota_camera_center")
+            --     hero:AddNewModifier(caster, ability, "modifier_stunned", modifier_table) 
+            -- end
         end
     end    
 
