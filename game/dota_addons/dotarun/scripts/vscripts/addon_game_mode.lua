7d27bfc03ce0d63155d10395693ab729e2ab5639
Okokookokok <2278040+mathiastj@users.noreply.github.com>
2014-09-10 21:33:55 +0200

    better restart, fix trap

--- game/dota_addons/dotarun/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/dotarun/scripts/vscripts/addon_game_mode.lua
@@ -48,14 +48,13 @@ function Activate()
 end
 
 function CDotaRun:InitGameMode()
+	self.TaTrapFired = false
 	self.itemList = { "item_blink", "item_cyclone", "item_shivas_guard", "item_sheepstick", "item_ancient_janggo", "item_smoke_of_deceit", "item_rod_of_atos"}
 	self.spellList = {"mirana_arrow_custom", "mirana_leap_custom", "venomancer_venomous_gale_custom", "dark_seer_surge_custom", "jakiro_ice_path_custom", 
 	"batrider_flamebreak_custom", "ancient_apparition_ice_vortex_custom", "gyrocopter_homing_missile_custom", "obsidian_destroyer_astral_imprisonment_custom", "pudge_meat_hook_custom"}
 
-	self.laps = {}
-	for i =1,5 do
-		self.laps[i] = 0
-	end
+	self.goodLaps = 0
+	self.badLaps = 0
 
 	-- GameRules:SetSameHeroSelectionEnabled( true )
 	-- DebugDrawText(Vector(-5464,-6529,20), "Get items and abilities by running through these", false, -1) 
@@ -101,6 +100,10 @@ function CDotaRun:InitGameMode()
 	print( "Dotarun has literally loaded." )
 
 	
+end
+
+function CDotaRun:ResetRound()
+	GameRules.dotaRun.TaTrapFired = false
 end
 
 -- function ApplyCooldownReduction( _, event )
