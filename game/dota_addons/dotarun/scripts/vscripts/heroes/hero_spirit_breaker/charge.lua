7da1d77902e06b65f4c8e93e7a0155a952162a21
mathiastj <2278040+mathiastj@users.noreply.github.com>
2016-09-09 12:50:59 +0200

    first draft spirit breaker working. Needs: change target if someone else is first, delete him after he is done

--- game/dota_addons/dotarun/scripts/vscripts/heroes/hero_spirit_breaker/charge.lua
+++ game/dota_addons/dotarun/scripts/vscripts/heroes/hero_spirit_breaker/charge.lua
@@ -1,6 +1,5 @@
 function Charge(keys)
 	local caster = keys.caster
-	local target = keys.target
 
 	charger = CreateUnitByName("charger", caster:GetAbsOrigin(), true, nil, nil, 1)
 	local chargeAbility = charger:FindAbilityByName("spirit_breaker_charge_of_darkness_custom")
@@ -9,17 +8,23 @@ function Charge(keys)
 
 	local playerID = GameRules.dotaRun.leadingPlayerID
 	local player = PlayerResource:GetPlayer(playerID)
-	-- local target = 0
-	-- if (player ~= nil) then
-	-- 	local hero = PlayerResource:GetPlayer(playerID):GetAssignedHero()
-	-- 	if (hero ~= nil) then
-	-- 		target = hero
-	-- 	end
-	-- end
+	local target = 0
+	if (player ~= nil) then
+		local hero = PlayerResource:GetPlayer(playerID):GetAssignedHero()
+		if (hero ~= nil) then
+			target = hero
+		end
+	end
+
+	Timers:CreateTimer(0.06, function()
+		charger:CastAbilityOnTarget(target, chargeAbility, target:GetPlayerOwnerID())
+		return 
+	end
+	)
 	
 
-	local targets = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE, FIND_CLOSEST, false)
-	charger:CastAbilityOnTarget(targets[1], chargeAbility, targets[1]:GetPlayerOwnerID())
+	--local targets = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE, FIND_CLOSEST, false)
+	--charger:CastAbilityOnTarget(targets[1], chargeAbility, targets[1]:GetPlayerOwnerID())
 	-- for i = 1, #targets do
 	-- 	if targets[i] ~= target then --avoid dealing twice the damage
 	-- 		local damageTable = {
